d674d237695eaf785c75e5a9a21a58e2d5f31b38
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-09 11:46:42 +0300

    add array filter method functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arr,filterValue) {
+    return arr.filter(val => val.toLowerCase() === filterValue.toLowerCase())
+}
+
+function fuzzyMatch(arr, filterValue) {
+    return arr.filter(val => val.slice(0,2) === filterValue.slice(0,2))
+}
+
+function matchName(arr, filterValue) {
+    return arr.filter(val => val.name === filterValue)
+}
